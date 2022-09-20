28f55fd38280484a7bb9814b7c6e05d0e761e8fb
Kótai Dániel <dkotai66@gmail.com>
2022-09-20 13:24:48 +0200

    lista kesz

--- lista.js
+++ lista.js
@@ -1,1 +1,22 @@
 lista = [];
+
+let szam = prompt("Adj meg egy számot: ")
+
+function hozzaad(szam){
+    if(szam<0) {
+        console.log("Hiba")
+    }
+    else {
+        lista.add(szam);
+    }
+}
+
+
+let db= 0;
+
+function atlag(lista){
+    for(var i=0;i<lista.length;i++) {
+        let atlag = (lista[i]+lista[i])/db;
+    }
+    
+}
